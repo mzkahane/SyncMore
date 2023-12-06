eb3da3e908cb0134b9c98bd010ce43221bd93099
AndrewLiu666 <aliu26@dons.usfca.edu>
2023-12-06 08:51:29 -0800

    remove unneccessary folder

--- user/models.py
+++ user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
 
 class Supervisor(models.Model):
@@ -13,6 +15,37 @@ class Supervisor(models.Model):
     updated_time = models.DateTimeField('updated_time', auto_now_add=True, null=True)
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        """
+        Create and save a regular User with the given username and password.
+        """
+        if not username:
+            raise ValueError(_('The Username must be set'))
+        user = self.model(Username=username, **extra_fields)
+        user.set_password(password)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, Username, password=None, **extra_fields):
+        """
+        Create and save a SuperUser with the given username and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(Username, password, **extra_fields)
+
+    def get_by_natural_key(self, username):
+        return self.get(Username=username)
+
+
 # Create your models here.
 class User(AbstractBaseUser, PermissionsMixin):
     First_Name = models.CharField('First_Name', max_length=30, null=True)
@@ -21,7 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     Username = models.CharField('Username', max_length=30, unique=True)
 
-    password = models.CharField('password', max_length=32)
+    password = models.CharField('password', max_length=128)
 
     address = models.CharField('address', max_length=255, default="San Francisco", null=True)
 
@@ -39,15 +72,20 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     second_password = models.IntegerField('second_password', null=True)
 
+    objects = UserManager()
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'Username'
 
-    REQUIRED_FIELDS = ['First_name', 'Last_name']
+    REQUIRED_FIELDS = ['First_Name', 'Last_Name']
 
 
 class Phone(models.Model):
     phone_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='phone_user')
 
-    phone = models.IntegerField('phone', default=0000000000, null=True)
+    phone = models.IntegerField('phone', default=00000000000000000000, null=True)
 
     created_time = models.DateTimeField('created_time', auto_now_add=True, null=True)
 
