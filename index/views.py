1f2108411c8c36dfce30954934390d9e2896cfee
lychee <vickiyoung112@gmail.com>
2023-12-07 15:06:20 -0800

    user app comments add

--- index/views.py
+++ index/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
-from django.core.mail import send_mail
-from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 
 
-# index view
+# handles requests to view the Home Page
 def index_view(request):
     return render(request, 'index/index.html')
 
 
+# handles requests to view the Contact Us page and form submissions when users submit a form to email SyncMore.
+# It constructs a message from the information the user provides and sends an email to what is specified by the EMAIL_HOST_USER
 def contact_view(request):
     if request.method == 'POST':
+        # get information from the form submission
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
